091e96618e7230e0cf88baeae4325e428edf7e1a
Derrick Ung <derrickrung@yahoo.com>
2016-08-18 12:45:13 -0700

    validated the zipcode

--- compass.js
+++ compass.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 })
 
 
-
 var imgURL = chrome.extension.getURL('pin_icon.png');
 
 function loopDom() {  
@@ -15,16 +14,20 @@ function loopDom() {
 
         for (var j = 0; j < element.childNodes.length; j++) {
             var node = element.childNodes[j];
+            
             if (node.nodeType === 3) {
                 var text = JSON.stringify(node.nodeValue);
                 var justText = node.nodeValue;
                 var found = text.search(/\d{5}/g);
                 var stringd = text.slice(found, found + 5)
                 if(found !== -1){
-                console.log(stringd, element)
-                console.log(justText);
-                $(element).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"><img class="pin-icon" src="` + imgURL + `"></a>`);
+                    if(zipHash[stringd]) {
+                    console.log(element.innerHTML)
+                    console.log('zip exists')
+                
+                    $(element).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"><img class="pin-icon" src="` + imgURL + `"></a>`);
             	}
+                }
             };
         };
     }
