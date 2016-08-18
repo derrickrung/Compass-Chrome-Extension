62f75e7e629f8a67faf982c65645bd5d3cc21898
jaycetan <jaycetan@gmail.com>
2016-08-18 12:11:54 -0700

    fixed issues with manifest and updated logic to support zip detection

--- compass.js
+++ compass.js
@@ -4,27 +4,10 @@ $(document).ready(function() {
 })
 
 
-// function findElementByText(text) {  
-//     var regex = "/\d{5}/g"
-//     var containsRegex = "*:contains('/\d{5}/g')"
-//         // var containsRegex = "*:contains(/\d{5}/g)"
 
+var imgURL = chrome.extension.getURL('pin_icon.png');
 
-//     $(containsRegex).last().val(function(index, value) {
-//         let innerEl = this.innerHTML;
-//         let outterEl = this.outterHTML;
-//         console.log(innerEl)
-
-//         // $(currentEl).val(function(index, value) {
-//         //     console.log(value);
-//         // })
-
-//         $(this).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"> <img class="pin-icon" src="pin_icon.png"></a>`);
-
-//     })
-// };
-
-function loopDom() {  
+function loopDom() {  
     var elements = document.getElementsByTagName('*');
 
     for (var i = 0; i < elements.length; i++) {
@@ -33,10 +16,15 @@ function loopDom() {  
         for (var j = 0; j < element.childNodes.length; j++) {
             var node = element.childNodes[j];
             if (node.nodeType === 3) {
-                var text = node.nodeValue;
-                console.log('text',text);
-                console.log('element',element);
-                $(element).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"> <img class="pin-icon" src="pin_icon.png"></a>`);
+                var text = JSON.stringify(node.nodeValue);
+                var justText = node.nodeValue;
+                var found = text.search(/\d{5}/g);
+                var stringd = text.slice(found, found + 5)
+                if(found !== -1){
+                console.log(stringd, element)
+                console.log(justText);
+                $(element).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"><img class="pin-icon" src="` + imgURL + `"></a>`);
+            	}
             };
         };
     }
