8eafe6f1afc424ad03a9b5beac99bdec017b2ca2
jaycetan <jaycetan@gmail.com>
2016-08-18 09:35:40 -0700

    still refactoring js to search dom. made attempts to update manifest.json to support external png.

--- compass.js
+++ compass.js
@@ -1,11 +1,52 @@
 $(document).ready(function() {
-    findElementByText('Database Administrators');
+    findElementByText("91205");
+    loopDom();
 })
 
 
 function findElementByText(text) {  
-    var jSpot = $("*:contains(" + text + ")")      .filter(function() {
-        return $(this).children().length === 0;
-    }).parent();    
-    console.log(jSpot); 
+    var regex = "/\d{5}/g"
+    var containsRegex = "*:contains('/\d{5}/g')"
+        // var containsRegex = "*:contains(/\d{5}/g)"
+
+
+    $(containsRegex).last().val(function(index, value) {
+        let innerEl = this.innerHTML;
+        let outterEl = this.outterHTML;
+        console.log(innerEl)
+
+        // $(currentEl).val(function(index, value) {
+        //     console.log(value);
+        // })
+
+        $(this).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"> <img class="pin-icon" src="pin_icon.png"></a>`);
+
+    })
+};
+
+function loopDom() {  
+
+	var items = document.getElementsByTagName("*");
+for (var i = items.length; i--;) {
+    //do stuff
+    // console.log(items[i]);
+    console.log($(items[i]).last());
+}
+    // var regex = "/\d{5}/g"
+    // var containsRegex = "*:contains('/\d{5}/g')"
+    //     // var containsRegex = "*:contains(/\d{5}/g)"
+
+
+    // $(containsRegex).last().val(function(index, value) {
+    //     let innerEl = this.innerHTML;
+    //     let outterEl = this.outterHTML;
+    //     console.log(innerEl)
+
+    //     // $(currentEl).val(function(index, value) {
+    //     //     console.log(value);
+    //     // })
+
+    //     $(this).append(`<a target="_blank" href="https://www.youtube.com/" class="compass-url"> <img class="pin-icon" src="pin_icon.png"></a>`);
+
+    // })
 };
